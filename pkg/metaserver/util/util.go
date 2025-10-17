882d98eac2c7a8548c4ed4c2d4ff9b53e369f5ed
Travis Baker <travis.baker@brinker.com>
2025-10-17 16:15:12 -0500

    fixes "Leases" being parsed as "Leas"
    Signed-off-by: Travis Baker <travis.baker@brinker.com>

--- pkg/metaserver/util/util.go
+++ pkg/metaserver/util/util.go
@@ -85,6 +85,7 @@ func UnsafeKindToResource(k string) string {
 		"NodeStatus":                   "nodestatus",
 		"CustomResourceDefinition":     "customresourcedefinitions",
 		"CustomResourceDefinitionList": "customresourcedefinitionlist",
+		"Leases":                       "leases",
 	}
 	if v, isUnusual := unusualKindToResource[k]; isUnusual {
 		return v
